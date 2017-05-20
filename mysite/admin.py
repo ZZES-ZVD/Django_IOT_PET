a8059ad1ec746ebbbac8b3d9ec7299bc1f8f9512
zzes <kuanglong0312@sina.com>
2017-05-20 10:09:49 +0800

    设置admin_site 自定义页面标题

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import User
+from .models import Iotdata
+from django.contrib.admin import AdminSite
+from django.utils.translation import ugettext_lazy
 
 admin.site.register(User)
+admin.site.register(Iotdata)
+
 # Register your models here.
+class MyAdminSite(AdminSite):
+    site_title = ugettext_lazy('治电科技物联网后台管理')
+    site_header = ugettext_lazy('ZZES IOT FOR PET')
+
+admin_site = MyAdminSite()
+
+admin_site.register(User)
+admin_site.register(Iotdata)
