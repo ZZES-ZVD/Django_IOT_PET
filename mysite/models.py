de1815a5bfd219870d56910a3abd74043c8a591f
zzes <kuanglong0312@sina.com>
2017-05-20 10:10:12 +0800

    创建mysite_iotdata表

--- mysite/models.py
+++ mysite/models.py
@@ -6,3 +6,10 @@ class User(models.Model):
     password = models.CharField(max_length=50)
     email = models.EmailField()
 
+class Iotdata(models.Model):
+    tem = models.IntegerField(max_length=50)
+    hum = models.IntegerField(max_length=50)
+    door = models.IntegerField(max_length=10)
+    feng = models.IntegerField(max_length=10)
+    indoor = models.IntegerField(max_length=10)
+    time = models.CharField(max_length=50)
